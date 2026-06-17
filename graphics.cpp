2007d419673d12a5fff4fb609a72abf8547a8016
VrBoiiAI <luisa04721@gmail.com>
2026-06-17 14:25:55 -0700

    Updated variable names

--- graphics.cpp
+++ graphics.cpp
@@ -4,85 +4,105 @@ using namespace std;
 
 int main(){
     // Variable initialization
-    int a,b,c,d,m,op;
+    int operation, upward_start, upward_end, peak, downward_start, downward_end ;
     float x;
 
     // We get the function to calculate
     printf("\n[1] Triangular Function\n[2] Gamma Function\n[3] Trapezoidal Function\n[4] S Function\n\nWhich function do you want to represent?: ");
-    scanf("%d", &op);
+    scanf("%d", &operation);
 
-    switch (op){
+    switch (operation){
     case 1:
+
         // Get data
         printf("Enter the value of a (start of the upward slope): ");
-        scanf("%d", &a);
+        scanf("%d", &upward_start);
         printf("Enter the value of m (peak of the triangle): ");
-        scanf("%d", &m);
+        scanf("%d", &peak);
         printf("Enter the value of b (end of the downward slope): ");
-        scanf("%d", &b);
-        if (a < m && m < b){ // We verify that the function has coherent values
+        scanf("%d", &downward_end);
+
+        // We verify that the function has coherent values
+        if (upward_start < peak && peak < downward_end){
             printf("Enter the value of x to calculate: ");
             scanf("%f", &x);
+
             // We get the value of x and evaluate its corresponding y value based on where on the graphic it its
-            if (x <= a) printf("The value of x in %.2f is: 0", x);
-            else if (x <= m) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-a)/(b-m)));
-            else if (x <= b) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(b)-x)/(b-m)));
+            if (x <= upward_start) printf("The value of x in %.2f is: 0", x);
+            else if (x <= peak) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-upward_start)/(downward_end-peak)));
+            else if (x <= downward_end) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(downward_end)-x)/(downward_end-peak)));
             else printf("The value of x in %.2f is: 0", x);
+
         } else printf("The values of the limits dont match (should be a<m<b)");
         break;
 
     case 2:
+
         // Get data 
         printf("Enter the value of a (start of the upward slope): ");
-        scanf("%d", &a);
+        scanf("%d", &upward_start);
         printf("Enter the value of m (end of the upward slope): ");
-        scanf("%d", &m);
-        if (a < m){ // We verify that the function has coherent values
+        scanf("%d", &upward_end);
+
+        // We verify that the function has coherent values
+        if (upward_start < upward_end){
             printf("Enter the value of x to calculate: ");
             scanf("%f", &x);
+
             // We get the value of x and evaluate its corresponding y value based on where on the graphic it its
-            if (x <= a) printf("The value of x in %.2f is: 0", x);
-            else if (x < m) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-a)/(m-a)));
+            if (x <= upward_start) printf("The value of x in %.2f is: 0", x);
+            else if (x < upward_end) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-upward_start)/(upward_end-upward_start)));
             else printf("The value of x in %.2f is: 1", x);
+
         } else printf("The values of the limits dont match (should be a < m)");
         break;
 
     case 3:
+
         // Get data
         printf("Enter the value of a (start of the upward slope): ");
-        scanf("%d", &a);
+        scanf("%d", &upward_start);
         printf("Enter the value of b (end of the upward slope): ");
-        scanf("%d", &b);
+        scanf("%d", &upward_end);
         printf("Enter the value of c (start of the downward slope): ");
-        scanf("%d", &c);
+        scanf("%d", &downward_start);
         printf("Enter the value of d (end of the downward slope): ");
-        scanf("%d", &d);
-        if (a < b && b < c && c < d){ // We verify that the function has coherent values
+        scanf("%d", &downward_end);
+
+        // We verify that the function has coherent values
+        if (upward_start < upward_end && upward_end < downward_start && downward_start < downward_end){ // We verify that the function has coherent values
             printf("Enter the value of x to calculate: ");
             scanf("%f", &x);
+
             // We get the value of x and evaluate its corresponding y value based on where on the graphic it its
-            if (x <= a) printf("The value of x in %.2f is: 0", x);
-            else if (x <= b) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-a)/(b-a)));
-            else if (x <= c) printf("The value of x in %.2f is: 1", x);
-            else if (x <= d) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(d)-x)/(d-c)));
+            if (x <= upward_start) printf("The value of x in %.2f is: 0", x);
+            else if (x <= upward_end) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(x)-upward_start)/(upward_end-upward_start)));
+            else if (x <= downward_start) printf("The value of x in %.2f is: 1", x);
+            else if (x <= downward_end) printf("The value of x in %.2f is: %.2f", x, ((static_cast<float>(downward_end)-x)/(downward_end-downward_start)));
             else printf("The value of x in %.2f is: 0", x);
+
         } else printf("The values of the limits dont match (should be a<b<c<d)");
         break;
 
     case 4:
+
         // Get data
         printf("Enter the value of a (start of the slope): ");
-        scanf("%d", &a);
+        scanf("%d", &upward_start);
         printf("Enter the value of c (end of the slope): ");
-        scanf("%d", &c);
-        if (a < c){ // We verify that the function has coherent values
+        scanf("%d", &upward_end);
+
+        // We verify that the function has coherent values
+        if (upward_start < upward_end){
             printf("Enter the value of x to calculate: ");
             scanf("%f", &x);
+
             // We get the value of x and evaluate its corresponding y value based on where on the graphic it its
-            if (x < a) printf("The value of x in %.2f is: 0", x);
-            else if (x <= ((static_cast<float>(a)+c)/(2))) printf("The value of x in %.2f is: %.2f", x, (2*pow((((static_cast<float>(x)-a))/(c-a)),2)));
-            else if (x <= c) printf("The value of x in %.2f is: %.2f", x, (1-(2*pow((static_cast<float>(c)-x)/(c-a),2))));
+            if (x < upward_start) printf("The value of x in %.2f is: 0", x);
+            else if (x <= ((static_cast<float>(upward_start)+upward_end)/(2))) printf("The value of x in %.2f is: %.2f", x, (2*pow((((static_cast<float>(x)-upward_start))/(upward_end-upward_start)),2)));
+            else if (x <= upward_end) printf("The value of x in %.2f is: %.2f", x, (1-(2*pow((static_cast<float>(upward_end)-x)/(upward_end-upward_start),2))));
             else printf("The value of x in %.2f is: 1", x);
+
         } else printf("The values of the limits dont match (should be a<c)");
         break;
     
